574fcd9b1438e71984456ebca8ac395cddeea2f1
Matilde Park <m@park.computer>
2022-09-07 16:26:03 -0700

    build: electron scaffolding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  console.log(process.env)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="bg-blue-400">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
