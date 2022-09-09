a95f8bc3a968dffd49a8df65ad233137b459bce6
yung calibri <yungcalibri@protonmail.com>
2022-09-09 16:15:58 -0400

    fix createStorageKey

--- src/state/util.js
+++ src/state/util.js
@@ -1,5 +1,5 @@
 export const useMockData = parseInt(process.env.REACT_APP_MOCK_DATA, 10);
 
-export const createStorageKey = name => `~${window.ship}/${window.desk}/${name}`;
+export const createStorageKey = name => `narthex/~${process.env.REACT_APP_SHIP}/${name}`;
 
 export const storageVersion = parseInt(process.env.REACT_APP_STORAGE_VERSION, 10);
