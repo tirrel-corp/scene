9ec05b3238fe7da6b9c50af2b57daefe3bd80838
Matilde Park <m@park.computer>
2022-09-08 22:45:18 -0700

    Screen: display rnd sessions, set focus on top bar click

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,3 +1,28 @@
-export default function Screen() {
-    return null
+import { Rnd } from 'react-rnd';
+
+export default function Screen({ windows, setWindows, selectedWindow, setSelectedWindow }) {
+    return <div className="grow">
+        {windows.map((win, index) => {
+            const href = 'glob' in win.chad ? `${process.env.REACT_APP_URL}/apps/${win.href.glob.base}` : `${process.env.REACT_APP_URL}${win.href.site}`
+            return <Rnd
+                key={win.title}
+                className="rounded-xl overflow-hidden"
+                style={{
+                    zIndex: win === selectedWindow ? 20 : index + 2
+                }}
+                default={{
+                    x: (index + 1) * 100,
+                    y: (index + 1) * 100,
+                    width: 640,
+                    height: 480,
+                }}
+                onClick={() => setSelectedWindow(win)}
+            >
+                <div className="w-full h-full p-2 flex flex-col bg-black cursor-default">
+                    <div onClick={() => console.log('click! div')} className="h-8 bg-black text-white w-full">{win.title}</div>
+                    <iframe onClick={() => console.log('click! iframe')} className="w-full h-full bg-white min-h-0" src={href} title={win.title} />
+                </div>
+            </Rnd>
+        })}
+    </div>
 }
