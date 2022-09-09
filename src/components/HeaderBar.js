5ceb0ca35e967abce7a3a47d3ba922488eab7369
yung calibri <yungcalibri@protonmail.com>
2022-09-09 15:50:00 -0400

    add icon, notifs button in top bar, crappy button styles

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,5 +1,12 @@
 export default function HeaderBar({ selectedWindow }) {
-    return <div className="w-full bg-[rgba(0,0,0,0.2)] flex justify-between items-center h-8 px-2 cursor-default">
-        <p>{selectedWindow.title || " "}</p>
+  return (
+    <div className="w-full bg-[rgba(0,0,0,0.2)] flex justify-between items-center px-2 py-1 cursor-default">
+      <p>{selectedWindow.title || " "}</p>
+      <button className="text-white flex">
+        <svg width="15" height="18" className="icon inline">
+          <use href="/icons.svg#bell" />
+        </svg>
+      </button>
     </div>
+  )
 }
