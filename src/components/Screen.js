cd7a6de8999455d442a7c881b35ba58b2c751bac
Matilde Park <m@park.computer>
2022-09-10 12:57:32 -0700

    Window: remove padding

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 export default function Screen({ windows, selectedWindow, hiddenWindow }) {
     return <div className="grow">
         {windows.value.map((win, index) => <Window
+            key={win.title}
             win={win}
             index={index}
             windows={windows}
@@ -18,7 +19,6 @@ const Window = ({ win, index, windows, selectedWindow, hiddenWindow }) => {
     const [key, setKey] = useState(0);
     const href = 'glob' in win.chad ? `${process.env.REACT_APP_URL}/apps/${win.href.glob.base}` : `${process.env.REACT_APP_URL}${win.href.site}`;
     return <Rnd
-        key={win.title}
         className="rounded-xl overflow-hidden shadow-md shadow-[rgba(0,0,0,0.1]"
         style={{
             zIndex: ([...selectedWindow.value].reverse().indexOf(win) + 1) * 10,
@@ -32,7 +32,7 @@ const Window = ({ win, index, windows, selectedWindow, hiddenWindow }) => {
         }}
         onMouseDown={() => selectedWindow.set([win, ...selectedWindow.value.filter((e) => e !== win)])}
     >
-        <div className="w-full h-full p-2 flex flex-col bg-[rgba(0,0,0,0.1)] cursor-default">
+        <div className="w-full h-full flex flex-col bg-[rgba(0,0,0,0.1)] cursor-default">
             <div className="h-8 bg-transparent text-white w-full flex justify-between items-center px-1">
                 <div className="flex space-x-2 items-center">
                     <a
