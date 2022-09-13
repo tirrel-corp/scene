2f22f53c30999ae7b9d3e1c6ac2df2421b46939f
yung calibri <yungcalibri@protonmail.com>
2022-09-13 18:49:01 -0400

    fix rendering issues

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -15,28 +15,30 @@ const Notifications = props => {
       id="notifications"
       class={`notifications text-zinc-200 ${visible ? 'shown' : ''}`}
     >
-      {Object.values(unseen)
-        .sort((a, b) => b.time - a.time)
-        .filter(n => !!n?.body?.[0])
-        .filter(n => !!charges?.[n.bin.place.desk])
-        .map(n => ([n, charges[n.bin.place.desk]]))
-        .map(([n, charge], idx) => (
-          <Notification key={idx} className="unseen" {...{notification: n, charge}}>
-            Unseen Notification {idx}
-          </Notification>
-        ))
-      }
-      {Object.values(seen)
-        .sort((a, b) => b.time - a.time)
-        .filter(n => !!n?.body?.[0])
-        .filter(n => !!charges?.[n.bin.place.desk])
-        .map(n => ([n, charges[n.bin.place.desk]]))
-        .map(([n, charge], idx) => (
-          <Notification key={idx} {...{notification: n, charge}}>
-            Seen Notification {idx}
-          </Notification>
-        ))
-      }
+      <section>
+        {Object.values(unseen)
+          .sort((a, b) => b.time - a.time)
+          .filter(n => !!n?.body?.[0])
+          .filter(n => !!charges?.[n.bin.place.desk])
+          .map(n => ([n, charges[n.bin.place.desk]]))
+          .map(([n, charge], idx) => (
+            <Notification key={idx} className="unseen" {...{notification: n, charge}}>
+              Unseen Notification {idx}
+            </Notification>
+          ))
+        }
+        {Object.values(seen)
+          .sort((a, b) => b.time - a.time)
+          .filter(n => !!n?.body?.[0])
+          .filter(n => !!charges?.[n.bin.place.desk])
+          .map(n => ([n, charges[n.bin.place.desk]]))
+          .map(([n, charge], idx) => (
+            <Notification key={idx} {...{notification: n, charge}}>
+              Seen Notification {idx}
+            </Notification>
+          ))
+        }
+      </section>
     </div>
   );
 }
@@ -59,19 +61,23 @@ const Notification = props => {
   };
 
   return (
-    <div className={`notification bg-neutral-700 text-white ${className}`}>
+    <div
+      onClick={() => console.debug(notification.body?.[0])}
+      className={`notification bg-neutral-700 text-white ${className}`}>
       <header>
         <DocketImage {...charge} />
         <h2>{charge?.title || notification.bin.place.desk}</h2>
-        <button className="archive ml-auto">
+        <button className="archive ml-auto" onClick={archiveNoFollow}>
           <svg width="9" height="9" className="icon inline">
             <use href="/icons.svg#cross-circle" />
           </svg>
         </button>
       </header>
       <article>
-        <h2>{notification.body[0].title.map(i => i.text).join()}</h2>
-        {_.take(contents, MAX_CONTENTS).map(c => (<p key={c}>{c}</p>))}
+        <h2>{notification.body[0].title.map(i =>
+          i.ship ? (<span className="ship">{i.ship}</span>) : (<span>{i.text}</span>)
+        )}</h2>
+        {_.take(contents, MAX_CONTENTS).map((c, idx) => (<p key={idx}>{c.text}</p>))}
         {contents.length > MAX_CONTENTS && (
           <p>...and {contents.length - MAX_CONTENTS} more</p>
         )}
