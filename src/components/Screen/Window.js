6b7a3358047d943933049420faff0dcd45212f03
Matilde Park <m@park.computer>
2022-09-14 20:27:26 -0700

    Window: add icons

--- src/components/Screen/Window.js
+++ src/components/Screen/Window.js
@@ -1,6 +1,9 @@
 import { Rnd } from 'react-rnd';
 import { useState } from 'react';
 import cn from 'classnames';
+import CloseIcon from '../icons/close';
+import MinimizeIcon from '../icons/minimize';
+import RefreshIcon from '../icons/refresh';
 
 export const Window = ({ win, index, windows, selectedWindow, launchOpen, hiddenWindow }) => {
     const [key, setKey] = useState(0);
@@ -23,26 +26,26 @@ export const Window = ({ win, index, windows, selectedWindow, launchOpen, hidden
                 className="h-8 bg-transparent text-white w-full flex justify-between items-center px-1"
                 onMouseDown={() => selectedWindow.set([win, ...selectedWindow.value.filter((e) => e !== win)])}
             >
-                <div className="flex space-x-2 items-center">
+                <div className="pl-2 flex space-x-2 items-center">
                     <a
-                        className="text-black"
+                        className="text-black cursor-pointer hover:opacity-80"
                         onClick={() => setKey(key + 1)}
                     >
-                        refresh
+                        <RefreshIcon />
                     </a>
                     <a
-                        className="text-black"
+                        className="text-black cursor-pointer hover:opacity-80"
                         onClick={() => hiddenWindow.set([...hiddenWindow.value, win])}>
-                        _
+                        <MinimizeIcon />
                     </a>
                     <a
-                        className="text-black"
+                        className="text-black bg-white overflow-hidden rounded-[100px] cursor-pointer hover:opacity-80"
                         onClick={() => {
                             windows.set(windows.value.filter((e) => e !== win));
                             selectedWindow.set(selectedWindow.value.filter((e) => e !== win))
                         }}
                     >
-                        x
+                        <CloseIcon />
                     </a>
                 </div>
             </div>
