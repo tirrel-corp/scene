aa0c8eafb9d7c9047e38dcedae7049c99763990b
yung calibri <yungcalibri@protonmail.com>
2022-09-16 14:24:18 -0400

    validity check

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -14,8 +14,18 @@ const HamburgerMenu = props => {
   );
 };
 
+function isValidUrl(input) {
+  try {
+    new URL(input);
+  } catch (err) {
+    return false;
+  }
+  return true;
+}
+
 const BackgroundInput = props => {
   const [value, setValue] = useState('');
+  const validity = isValidUrl(value);
 
   return (
     <div className="rounded bg-neutral-500 text-white overflow-hidden">
@@ -27,7 +37,9 @@ const BackgroundInput = props => {
           <input
             type="url"
             id="background-input"
-            className={`block grow rounded p-1 text-small`}
+            className={`block grow rounded p-1 text-small text-black border-2 ${
+              !!value && !validity ? 'border-red-400' : 'border-transparent'
+            }`}
             placeholder="https://image.host/image.jpg"
             onChange={ev => setValue(ev.target.value)}
           />
