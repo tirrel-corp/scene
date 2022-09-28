85887650fac1d296d0b26cd7d76a06c5a8e967f2
yung calibri <yungcalibri@protonmail.com>
2022-09-28 18:46:42 -0400

    onboarding payment details form with react-hook-form
    inputs dont change border color when invalid

--- src/components/Onboarding/detail.js
+++ src/components/Onboarding/detail.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
-import { Link, useOutletContext } from "react-router-dom";
+import { Link, useOutletContext, useNavigate } from "react-router-dom";
+import { useForm } from 'react-hook-form';
 import Sigil from "../sigil";
 
 const validate = (fullName, line1, city, state, postal, email, setError) => {
@@ -13,14 +14,22 @@ const validate = (fullName, line1, city, state, postal, email, setError) => {
 
 export default function PayDetailScreen() {
     const { planet, credit, setCredit } = useOutletContext();
-    const [fullName, setFullname] = useState("");
-    const [line1, setLine1] = useState("");
-    const [line2, setLine2] = useState("");
-    const [city, setCity] = useState("");
-    const [state, setSt] = useState("");
-    const [postal, setPostal] = useState("");
-    const [email, setEmail] = useState("");
-    const [error, setError] = useState(false);
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({
+        mode: 'onBlur',
+        defaultValues: {
+            name: '',
+            line1: '',
+            line2: '',
+            city: '',
+            state: '',
+            postal: '',
+            email: '',
+        },
+    });
+    const email = watch('email');
+
+    const navigate = useNavigate();
 
     return <div className="grow flex justify-center items-center text-white">
         <div className="flex space-x-12">
@@ -31,18 +40,28 @@ export default function PayDetailScreen() {
                 </div>
                 <p>{planet}</p>
             </div>
-            <div className="flex-col space-y-4">
+            <form
+                className="flex-col space-y-4"
+                onSubmit={handleSubmit((data) => {
+                    setCredit(data);
+                    navigate('/new/pay');
+                })}
+            >
                 <p>Full name</p>
                 <input
-                    className="w-full p-3 border-b bg-transparent invalid:border-red-500 placeholder-shown:!border-white"
+                    className="w-full p-3 border-b bg-transparent placeholder-shown:!border-white invalid:border-red-500"
                     type="text"
                     placeholder="John Smith"
                     autoComplete="cc-given-name"
                     id="name"
-                    pattern="(?:(\w+-?\w+)) (?:(\w+))(?: (\w+))?$"
-                    required="true"
-                    onChange={(e) => setFullname(e.target.value)}
-                    value={fullName}
+                    {...register('name', {
+                        required: true,
+                        minLength: 1,
+                        pattern: {
+                            value: /(?:(\w+-?\w+)) (?:(\w+))(?: (\w+))?$/,
+                            message: 'Full name as it appears on your credit card',
+                        },
+                    })}
                 />
                 <p>Billing address</p>
                 <input
@@ -51,17 +70,15 @@ export default function PayDetailScreen() {
                     placeholder="200 Cherry Ln"
                     autoComplete="address-level1"
                     id="address1"
-                    required="true"
-                    onChange={(e) => setLine1(e.target.value)}
-                    value={line1}
+                    {...register('line1', { required: true })}
                 />
                 <input
                     className="w-full p-3 border-b   bg-transparent"
                     type="text"
+                    placeholder="Apt G"
                     autoComplete="address-level2"
                     id="address2"
-                    onChange={(e) => setLine2(e.target.value)}
-                    value={line2}
+                    {...register('line2')}
                 />
                 <p>City</p>
                 <input
@@ -69,9 +86,7 @@ export default function PayDetailScreen() {
                     type="text"
                     placeholder="Verduria"
                     id="city"
-                    required="true"
-                    onChange={(e) => setCity(e.target.value)}
-                    value={city}
+                    {...register('city', { required: true })}
                 />
                 <div className="flex justify-between">
                     <div className="flex flex-col space-y-2">
@@ -82,10 +97,10 @@ export default function PayDetailScreen() {
                             placeholder="MN"
                             autoComplete="address-level1"
                             id="state"
-                            maxLength={2}
-                            required="true"
-                            onChange={(e) => setSt(e.target.value)}
-                            value={state}
+                            {...register('state', {
+                                required: true,
+                                maxLength: 2,
+                            })}
                         />
                     </div>
                     <div className="flex flex-col space-y-2">
@@ -96,43 +111,39 @@ export default function PayDetailScreen() {
                             placeholder="88888"
                             autoComplete="postal-code"
                             id="postal"
-                            minLength={5}
-                            maxLength={5}
-                            required="true"
-                            onChange={(e) => setPostal(e.target.value)}
-                            value={postal}
+                            {...register('postal', {
+                                required: true,
+                                minLength: 5,
+                                maxLength: 5,
+                            })}
                         />
                     </div>
                 </div>
                 <p>Email address</p>
                 <input
                     className="w-full p-3 border-b   bg-transparent invalid:border-red-500 placeholder-shown:!border-white"
-                    type="text"
+                    type="email"
                     placeholder="john@apple.seed"
                     id="email"
-                    required="true"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
+                    {...register('email', { required: true })}
                 />
                 <p>Confirm email address</p>
                 <input
                     className="w-full p-3 border-b bg-transparent invalid:border-red-500 placeholder-shown:!border-white"
                     placeholder="john@apple.seed"
-                    required="true"
-                    type="text"
+                    required
+                    type="email"
                     pattern={email}
                 />
-                <Link
-                    to="/new/pay"
-                    onClick={(e) => {
-                        return validate(fullName, line1, city, state, postal, email, setError)
-                            ? setCredit({ fullName, line1, line2, city, state, postal, email })
-                            : e.preventDefault()
-                    }}>
-                    <a className="mt-8 cursor-pointer block rounded-full px-4 py-2 bg-[rgba(217,217,217,0.2)] text-white hover:brightness-110 text-xl text-center">Continue</a>
-                </Link>
-                {error && <p className="text-red-600">Please ensure all fields are correct.</p>}
-            </div>
+                <div className="flex justify-center">
+                    <button className="mt-8 cursor-pointer block rounded-full px-4 py-2 bg-[rgba(217,217,217,0.2)] text-white hover:brightness-110 text-xl text-center">Continue</button>
+                </div>
+                {Object.keys(errors).length > 0 && (
+                    <p className="text-red-600">
+                        Please ensure all fields are correct.
+                    </p>
+                )}
+            </form>
         </div>
     </div>
 }
