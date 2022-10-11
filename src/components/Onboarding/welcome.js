549f6bb9ce0d8a81366e1bb3e3d477d1e501808f
yung calibri <yungcalibri@protonmail.com>
2022-10-11 18:08:01 -0400

    make sign-in flow a form so that it will submit on enter

--- src/components/Onboarding/welcome.js
+++ src/components/Onboarding/welcome.js
@@ -32,10 +32,18 @@ export default function Welcome() {
         <p className="text-xl  text-white">Welcome to Planet One</p>
         <div className="flex flex-col space-y-4 items-center">
             <p className="text-white">To sign up or log in, enter your email to receive a sign-in link.</p>
-            {!sent ? <div className="flex min-w-0 w-full space-x-4 items-center">
-                <input className="text-white bg-transparent border-b grow p-2" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <button className="bg-white hover:bg-transparent hover:text-white hover:!opacity-100 text-[#6184FF] p-2" onClick={() => sendEmail()}>Send</button>
-            </div> : <p className="text-white">Check your email for a login link.</p>}
+            {!sent ? (
+                <form className="flex min-w-0 w-full space-x-4 items-center">
+                    <input className="text-white bg-transparent border-b grow p-2" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <button
+                        className="bg-white hover:bg-transparent hover:text-white hover:!opacity-100 text-[#6184FF] p-2"
+                        onClick={() => sendEmail()}>
+                            Send
+                    </button>
+                </form>
+            ) : (
+                <p className="text-white">Check your email for a login link.</p>
+            )}
         </div>
         {process.env.REACT_APP_DEBUG && <Link to="/debug"><button className="text-white">Sign in with a non-Tirrel ship</button></Link>}
     </div>
