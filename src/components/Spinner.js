b3be026be95f3c674f0ae8978ef4a110b3128b50
yung calibri <yungcalibri@protonmail.com>
2022-10-18 14:43:05 -0400

    fixup! make a loading spinner component for reuse

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import Sigil from './sigil';
 export default function Spinner(props) {
   const { patp = "~tirrel", color = "#3045B1" } = props;
   return (
-    <div className="spinner">
+    <div className="spinner overflow-hidden rounded-xl">
       <Sigil patp={patp} color={color} />
     </div>
   )
