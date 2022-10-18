af00aae1ba4d87891ebddd61086471fdf4fd72ec
yung calibri <yungcalibri@protonmail.com>
2022-10-18 16:42:55 -0400

    add payment lib

---
+++ src/lib/payment.js
@@ -0,0 +1,67 @@
+import * as openpgp from 'openpgp';
+import { v4 as uuidv4 } from 'uuid';
+import { publicKey, tirrelServer } from './constants';
+
+export const createCardUrl = `${tirrelServer}/third/create-card`
+export const buyUrl = `${tirrelServer}/third/buy`
+
+export const createCard = async (cardDetails, billingDetails) => {
+  const decodedPublicKey = await openpgp.readKey({ armoredKey: atob(publicKey) });
+
+  const encryptedData = await openpgp.encrypt({
+    message: await openpgp.createMessage({ text: JSON.stringify(cardDetails) }),
+    encryptionKeys: decodedPublicKey,
+  }).then(message => btoa(message));
+
+  let data = {
+    billingDetails,
+    email: billingDetails.email,
+    expMonth: cardDetails.expMonth,
+    expYear: cardDetails.expYear,
+    encryptedData,
+    idempotencyKey: uuidv4(),
+    keyId: 'key1', // TODO 
+  }
+  const target = createCardUrl;
+  await fetch(target, {
+    method: 'POST',
+    headers: { 'Content-type': 'text/json' },
+    body: JSON.stringify(data)
+  });
+
+  return;
+}
+
+export const buyPlanet = async (session, planet) => {
+  const res = await fetch(buyUrl, {
+    method: 'POST',
+    headers: {'Content-type': 'text/json'},
+    body: JSON.stringify({ session, planet })
+  });
+  return res;
+};
+
+// export const createPayment = async (session, cardId, cvv) => {
+//   const toEncrypt = { cvv };
+//   const decodedPublicKey = await openpgp.readKey({ armoredKey: atob(publicKey) });
+//   const encryptedData = await openpgp.encrypt({
+//     message: await openpgp.createMessage({ text: JSON.stringify(toEncrypt) }),
+//     encryptionKeys: decodedPublicKey,
+//   }).then(message => btoa(message));
+
+//   const create = buyUrl;
+//   await fetch(create, {
+//     method: 'POST',
+//     headers: { 'Content-type': 'text/json' },
+//     body: JSON.stringify({
+//       idempotencyKey: session,
+//       cardId,
+//       encryptedData,
+//     })
+//   });
+
+//   return;
+// };
+
+export const isBillingDetailsError = ({ code }) =>
+  !([1100, 1110, 1109, 1032].includes(Number(code)));
