40b56a169ca5f844538e6ee00454f56c5952266e
yung calibri <yungcalibri@protonmail.com>
2022-10-19 13:44:51 -0400

    visual improvements for the dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -24,21 +24,22 @@ const Dashboard = props => {
     if (ships?.shipStatus === 'running') { return; }
     const shipStatusPoll = setInterval(() => updateAccountState(), 5000)
     return () => clearInterval(shipStatusPoll);
-  }, [accountState.ships]);
+  }, [accountState?.ships]);
 
   return (
-    <div className="grow flex flex-col space-y-12 items-center justify-center antialiased font-inter">
-      <div className="flex flex-col gap-4">
+    <div className="grow flex flex-col space-y-12 items-center justify-center antialiased font-inter text-white">
+      <div className="flex flex-col items-center gap-4">
         {accountState?.ships?.patp && (<>
-          <div className="spinner">
+          <div className={`overflow-hidden rounded-xl ${accountState?.ships?.shipStatus !== 'running' ? 'spinner' : ''}`}>
             <Sigil patp={accountState.ships.patp} color="#6184FF" />
           </div>
-          <p>{accountState.ships.patp}</p>
+          <p className="mt-4 text-xl">{accountState.ships.patp}</p>
         </>)}
         {accountState?.ships?.shipStatus !== 'running' && (
-          <button disabled className="block rounded-full px-4 py-2 bg-[rgba(217,217,217,0.2)] text-white text-xl text-center cursor-not-allowed">
-            Getting Ready...
-          </button>
+          <div className="block rounded-lg px-8 py-4 bg-[rgba(217,217,217,0.2)] text-white text-center">
+            <p>Getting your ship ready.</p>
+            <p>This may take up to ten minutes.</p>
+          </div>
         )}
         {accountState?.ships?.shipStatus === 'running' && (
           <button onClick={respawn} className="block rounded-full px-4 py-2 bg-[rgba(217,217,217,0.2)] text-white text-xl text-center cursor-not-allowed">
