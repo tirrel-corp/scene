4abfece9d97ce397bf1a43be72a42c90410024a0
yung calibri <yungcalibri@protonmail.com>
2022-11-01 20:46:02 -0400

    touch up the debug form

--- src/components/Onboarding/debug.js
+++ src/components/Onboarding/debug.js
@@ -14,38 +14,40 @@ export default function DebugMenu() {
   }
 
   return (
-    <div className="flex flex-col space-y-4 text-white rounded-lg p-5 bg-[#000A]">
+    <div className="flex flex-col space-y-5 text-white rounded-lg p-5 bg-[#000A]">
       <div className="flex flex-row">
         <button className="px-4" onClick={() => navigate(-1)}>
           &#10094;&nbsp;Back
         </button>
       </div>
       <p>To sign in to your ship, please provide the following details.</p>
-      <p>Ship name (without the leading ~)</p>
-      <input
-        type="text"
-        className="bg-transparent border-b p-2"
-        placeholder="sidfus-tirlyx"
-        value={ship}
-        onChange={(e) => setShip(e.target.value)}
+      <form className="flex flex-col space-y-4">
+        <p>Ship name (without the leading ~)</p>
+        <input
+            type="text"
+            className="bg-transparent border-b p-2"
+            placeholder="sidfus-tirlyx"
+            value={ship}
+            onChange={(e) => setShip(e.target.value)}
         />
-      <p>URL</p>
-      <input
-        type="text"
-        className="bg-transparent border-b p-2"
-        placeholder="https://sidfus-tirlyx.arvo.planet.one"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
+        <p>URL</p>
+        <input
+            type="text"
+            className="bg-transparent border-b p-2"
+            placeholder="https://sidfus-tirlyx.arvo.planet.one"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
         />
-      <p>Access Key (+code)</p>
-      <input
-        type="text"
-        className="bg-transparent border-b p-2"
-        placeholder="lidlut-tabwed-pillex-ridrup"
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
+        <p>Access Key (+code)</p>
+        <input
+            type="text"
+            className="bg-transparent border-b p-2"
+            placeholder="lidlut-tabwed-pillex-ridrup"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
         />
-      <button onClick={() => respawn()}>Relaunch</button>
+        <button onClick={() => respawn()}>Relaunch</button>
+      </form>
     </div>
   )
 }
