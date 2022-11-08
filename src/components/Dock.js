543cea86b4c5188c98b008ec9f9e6aecd60bf9b1
yung calibri <yungcalibri@protonmail.com>
2022-11-08 15:15:10 -0500

    darken background for header bar, dock, and launchpad

--- src/components/Dock.js
+++ src/components/Dock.js
@@ -2,7 +2,7 @@ import Tippy from '@tippyjs/react';
 import LaunchpadIcon from "./icons/launchpad"
 
 export default function Dock({ windows, focusByCharge, launchOpen }) {
-    return <div className="bg-[rgba(0,0,0,0.5)] backdrop-blur-sm text-white w-fit self-center items-center p-2 flex rounded-t-md shadow-sm shadow-[rgba(0,0,0,0.15)] border border-[rgba(0,0,0,0.15)]">
+    return <div className="bg-[rgba(0,0,0,0.7)] backdrop-blur-sm text-white w-fit self-center items-center p-2 flex rounded-t-md shadow-sm shadow-[rgba(0,0,0,0.15)] border border-[rgba(0,0,0,0.15)]">
         {windows.value.map((charge) => {
             return <Tippy key={charge.title} content={charge.title}>
                 <div
