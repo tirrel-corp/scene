8ffeb47017e8ac03f60999bf73ddc0dd56dad1e7
yung calibri <yungcalibri@protonmail.com>
2022-11-30 13:52:54 -0500

    prettier

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,17 +1,26 @@
-import Window from './Screen/Window';
+import Window from "./Screen/Window";
 
-export default function Screen({ windows, selectedWindow, hiddenWindow, launchOpen, children }) {
-    return <div className="grow">
-        {children}
-        {windows.value.map((win, index) => <Window
-            key={win.title}
-            win={win}
-            launchOpen={launchOpen}
-            index={index}
-            windows={windows}
-            selectedWindow={selectedWindow}
-            hiddenWindow={hiddenWindow}
-        />)
-        }
+export default function Screen({
+  windows,
+  selectedWindow,
+  hiddenWindow,
+  launchOpen,
+  children,
+}) {
+  return (
+    <div className="grow">
+      {children}
+      {windows.value.map((win, index) => (
+        <Window
+          key={win.title}
+          win={win}
+          launchOpen={launchOpen}
+          index={index}
+          windows={windows}
+          selectedWindow={selectedWindow}
+          hiddenWindow={hiddenWindow}
+        />
+      ))}
     </div>
+  );
 }
