2dc74a673bc22bb3c5745c0a2e705aa58aa9cc2f
Matilde Park <m@park.computer>
2023-01-16 14:58:50 -0600

    allies: add 'add' reducers for allies to prevent undefined + quirky behaviour

--- src/state/allies.js
+++ src/state/allies.js
@@ -1,10 +1,16 @@
 export const allyReducer = (state, action) => {
+    console.log(state);
+    console.log(action);
     switch (Object.keys(action)[0]) {
         case "ini":
             return action.ini;
         case "new":
             const { ship, alliance } = action.new;
             return { ...state, [ship]: alliance }
+        case "add":
+            return { ...state, [action.add]: [] }
+        default:
+            return state;
     }
 }
 
