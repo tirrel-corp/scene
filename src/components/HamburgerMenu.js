590a7bd88fe3d8a9f755f61b9fc0ab90a7891211
Matilde Park <m@park.computer>
2023-02-14 12:56:19 -0800

    HamburgerMenu, PlanetMenu: match

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import { set } from '../lib/background';
+import { useState, useContext } from 'react';
+import { set, whiteOrBlack } from '../lib/background';
+import { ThemeContext } from "../App"
 
 const HamburgerMenu = props => {
   const { nativeNotifs, setBgImage, visible } = props;
@@ -7,7 +8,7 @@ const HamburgerMenu = props => {
   return (
     <div
       id="hamburger"
-      className={`w-full max-w-[350px] z-20 absolute top-12 right-2 ${visible.value ? 'visible fade-in' : 'hidden'}`}
+      className={`w-full bg-[rgba(0,0,0,0.3)] p-2 rounded-xl max-w-[350px] z-[9999] absolute top-12 right-2 ${visible.value ? 'visible fade-in' : 'hidden'}`}
     >
       <section className="flex flex-col gap-2">
         <NotificationsToggle value={nativeNotifs.value} set={nativeNotifs.set} />
@@ -34,18 +35,31 @@ const BackgroundInput = props => {
   const { onSave, setBgImage } = props;
   const [value, setValue] = useState('');
   const validity = value === '' ? true : isValidUrl(value) && isImage(value);
+  const palette = useContext(ThemeContext);
+  const header = `rgb(${palette?.["Muted"]?.join(",") || "0,0,0"})`;
+  const content = `rgb(${palette?.["DarkMuted"]?.join(",") || "0,0,0"})`;
+  const text = whiteOrBlack(content);
 
   return (
-    <div className="rounded bg-neutral-500 text-white overflow-hidden">
-      <header className="bg-neutral-600 text-xs px-2 py-1">
+    <div className="rounded overflow-hidden"
+      style={{
+        backgroundColor: content,
+        color: text
+      }}
+    >
+      <header className="text-xs p-2"
+        style={{
+          backgroundColor: header
+        }}
+      >
         Background Image
       </header>
       <div className="flex flex-col gap-1 p-2">
-        <div className="flex gap-1">
+        <div className="flex space-x-2">
           <input
             type="url"
             id="background-input"
-            className={`block grow rounded p-1 text-small text-black border-2 ${!!value && !validity ? 'border-red-400' : 'border-transparent'
+            className={`block grow rounded p-2 text-xs text-black border-2 ${!!value && !validity ? 'border-red-400' : 'border-transparent'
               }`}
             placeholder="https://image.host/image.jpg"
             onChange={ev => setValue(ev.target.value)}
@@ -54,7 +68,12 @@ const BackgroundInput = props => {
           <button disabled={!validity} onClick={() => {
             set(value, setBgImage)
             onSave()
-          }}>
+          }}
+            style={{
+              backgroundColor: header,
+              borderColor: 'transparent'
+            }}
+          >
             Save
           </button>
         </div>
@@ -65,10 +84,22 @@ const BackgroundInput = props => {
 
 const NotificationsToggle = props => {
   const { value, set } = props;
+  const palette = useContext(ThemeContext);
+  const header = `rgb(${palette?.["Muted"]?.join(",") || "0,0,0"})`;
+  const content = `rgb(${palette?.["DarkMuted"]?.join(",") || "0,0,0"})`;
+  const text = whiteOrBlack(content);
+
   const toggle = () => set(!value);
   return (
-    <div className="rounded bg-neutral-500 text-white overflow-hidden">
-      <header className="bg-neutral-600 text-xs px-2 py-1">
+    <div className="rounded overflow-hidden"
+      style={{
+        backgroundColor: content,
+        color: text
+      }}>
+      <header className="text-xs p-2"
+        style={{
+          backgroundColor: header
+        }}>
         Native Notifications
       </header>
       <div className="flex flex-col gap-1 p-2">
@@ -81,7 +112,12 @@ const NotificationsToggle = props => {
               </span>
             </p>
           </div>
-          <button onClick={toggle}>
+          <button
+            onClick={toggle}
+            style={{
+              backgroundColor: header,
+              borderColor: 'transparent'
+            }}>
             {!value ? 'Enable' : 'Disable'}
           </button>
         </div>
--- src/components/PlanetMenu.js
+++ src/components/PlanetMenu.js
@@ -1,5 +1,8 @@
 import cn from "classnames";
 import { getAuth, clearAuth } from "../lib/auth";
+import { useContext } from 'react';
+import { whiteOrBlack } from '../lib/background';
+import { ThemeContext } from "../App"
 
 export default function PlanetMenu(props) {
   const { visible, updateAvailable, appVersion } = props;
@@ -7,12 +10,12 @@ export default function PlanetMenu(props) {
   return (
     <div
       id="planet-menu"
-      className={cn("text-white w-full max-w-[350px] z-20 absolute top-12 left-2", { "shown fade-in": !!visible.value, "hidden": !Boolean(visible.value) })}
+      className={cn("w-full max-w-[350px] z-[9999] absolute top-12 left-2 bg-[rgba(0,0,0,0.3)] p-2 rounded-xl ", { "shown fade-in": !!visible.value, "hidden": !Boolean(visible.value) })}
     >
       <section className="flex flex-col gap-2">
         {updateAvailable && <UpdatePrompt />}
         <LogOut ship={ship} handleLogout={() => clearAuth()} />
-        <div className="px-2 py-1 text-xs text-center">
+        <div className="px-2 py-1 text-xs text-center text-white">
           {!!appVersion && <p className="font-bold">Scene v{appVersion}</p>}
           <p>
             To file a bug report, or for other support concerns, email&nbsp;
@@ -33,17 +36,39 @@ export default function PlanetMenu(props) {
 
 const LogOut = (props) => {
   const { ship, handleLogout } = props;
+  const palette = useContext(ThemeContext);
+  const header = `rgb(${palette?.["Muted"]?.join(",") || "0,0,0"})`;
+  const content = `rgb(${palette?.["DarkMuted"]?.join(",") || "0,0,0"})`;
+  const text = whiteOrBlack(content);
+
   return (
-    <div className="rounded bg-neutral-500 overflow-hidden">
-      <header className="bg-neutral-600 text-xs px-2 py-1">Log Out</header>
+    <div className="rounded overflow-hidden"
+      style={{
+        backgroundColor: content,
+        color: text
+      }}>
+      <header className="text-xs p-2"
+        style={{
+          backgroundColor: header
+        }}>
+        Log Out
+      </header>
       <div className="flex flex-col gap-1 p-2">
         <div className="flex gap-1 items-center">
           <div className="grow">
-            <p>
+            <p className="text-xs">
               Log out of <code>~{ship}</code>
             </p>
           </div>
-          <button onClick={handleLogout}>Log Out</button>
+          <button
+            onClick={handleLogout}
+            style={{
+              backgroundColor: header,
+              borderColor: 'transparent'
+            }}
+          >
+            Log Out
+          </button>
         </div>
       </div>
     </div>
